981c9dccbc16f2890d61a3e7814c5ff76ac95afe
anoojlal <anoojl2@illinois.edu>
2023-02-27 15:58:23 -0500

    trying to fix deploy build

---
+++ build/precache-manifest.b6f970b457d09e5507edca62e212f0da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a552585ed84d54ba900e1da7aea3c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eda1d28e38571892070",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "2ca92048f57492c02dc4",
+    "url": "/static/css/main.7424d926.chunk.css"
+  },
+  {
+    "revision": "1eda1d28e38571892070",
+    "url": "/static/js/2.cd9b8360.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/static/js/2.cd9b8360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca92048f57492c02dc4",
+    "url": "/static/js/main.7a4b92f1.chunk.js"
+  },
+  {
+    "revision": "39312879d10a12638c55",
+    "url": "/static/js/runtime-main.905aa465.js"
+  }
+]);
